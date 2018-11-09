62a41e6ead4ae9468bf39e0f9e4e5b697a9b6b9c
William McCloskey <mcclow12@stanford.edu>
2018-11-08 17:44:08 -0800

    used sigmoid output, added labels to plots

--- pythonCode/deep_nn.py
+++ pythonCode/deep_nn.py
@@ -21,6 +21,7 @@ np.random.seed(1)
 
 # training data
 train   = data.sample(frac=0.8)
+pdb.set_trace()
 x_train = np.ndarray((len(train),3)); y_train = np.ndarray((len(train),1))
 
 x_train[:,0] = train['meanCp']; x_train[:,1] = train['k']
@@ -43,9 +44,9 @@ np.random.seed(1)
 #model.add(tf.keras.layers.Dense(units=1, activation='tanh'))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(units=10, activation = 'relu', input_shape = x_train.shape))
-model.add(tf.keras.layers.Dense(units=5, activation = 'relu', input_shape = x_train.shape))
-model.add(tf.keras.layers.Dense(units=3, activation = 'relu', input_shape = x_train.shape))
-model.add(tf.keras.layers.Dense(units=1, activation='tanh'))
+model.add(tf.keras.layers.Dense(units=5, activation = 'relu'))
+model.add(tf.keras.layers.Dense(units=3, activation = 'relu'))
+model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
 ## loss
 #model.compile(optimizer='rmsprop', loss='mse')
@@ -54,7 +55,7 @@ model.compile(optimizer='adam', loss='mse')
 # train
 np.random.seed(1)
 #model.fit(x_train, y_train, epochs=100)
-model.fit(x_train, y_train, epochs=300)
+model.fit(x_train, y_train, epochs=500)
 
 loss_and_metrics = model.evaluate(x_test, y_test)
 
@@ -76,14 +77,16 @@ plot(y_train, y_nn_train)
 plot(y_test,  y_nn_test)
 
 plt.figure(figsize=(10,5))
-plt.subplot(1,2,1)
+ax1 = plt.subplot(1,2,1)
 tile_A0.plot_RMSContour(y_nn_A0); tile_A180.plot_RMSContour(y_nn_A180)
 tile_B0.plot_RMSContour(y_nn_B0); tile_B180.plot_RMSContour(y_nn_B180)
+ax1.set_title(r'NN RMS Estimate, $\theta = 0^\circ, 180^\circ$')
 plt.colorbar()
 
-plt.subplot(1,2,2)
+ax2 = plt.subplot(1,2,2)
 tile_A0.plot_RMSContour(tile_A0.rmsCp); tile_A180.plot_RMSContour(tile_A180.rmsCp)
 tile_B0.plot_RMSContour(tile_B0.rmsCp); tile_B180.plot_RMSContour(tile_B180.rmsCp)
+ax2.set_title(r'Experimental RMS Estimate, $\theta = 0^\circ, 180^\circ$')
 plt.colorbar()
 
 plt.figure(figsize=(15,8))
@@ -93,4 +96,20 @@ tile_A180.plot_RMSProfiles(y_nn_A180)
 plt.figure(figsize=(15,8))
 tile_B0.plot_RMSProfiles(y_nn_B0)
 tile_B180.plot_RMSProfiles(y_nn_B180)
+
+
+plt.figure(figsize=(10,5))
+ax3=plt.subplot(1,2,1)
+tile_A20.plot_RMSContour(y_nn_A20);
+tile_B20.plot_RMSContour(y_nn_B0); 
+ax3.set_title(r'NN RMS Estimate, $\theta = 20^\circ$')
+plt.colorbar()
+
+ax4 = plt.subplot(1,2,2)
+tile_A20.plot_RMSContour(tile_A20.rmsCp);
+tile_B20.plot_RMSContour(tile_B20.rmsCp); 
+ax4.set_title(r'Experimental RMS Estimate, $\theta = 20^\circ$')
+plt.colorbar()
+
 plt.show()
+
